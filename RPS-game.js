9eb592c97502a1a8c231efca2d8989d2b8ac6fe2
Jlt100 <jauwanthms5@gmail.com>
2021-07-12 21:57:26 -0500

    updated RPS game javascript code

--- RPS-game.js
+++ RPS-game.js
@@ -1,20 +1,56 @@
-const playerSelection = "rock"; 
-const computerSelection = computerPlay();
-const rps =["rock", "paper", "scissors"];
+
+const rps =['rock', 'paper', 'scissors'];
 
 function computerPlay() {
 	let randomrps = rps[Math.floor(Math.random() * rps.length)];
 		return randomrps; 
 	}
+const computerSelection = computerPlay();
+playerInput = prompt("Choose 'rock', 'paper', or 'scissors'");
 
-function playRound( playerSelection, computerSelection) {
-	if (playerSelection == "rock" && computerSelection == "rock"){ 
-		console.log("It's a tie"); 
-		}else if (playerSelection == "rock" && computerSelection == "paper"){
-		console.log("You lose paper beats rock"); 
-		}else if (playerSelection == "rock" && computerSelection == "scissors"){
-		console.log("You win rock beats scissors"); 
+let userscore = 0;  
+let compscore = 0;
+
+	function playGame(playerInput, computerSelection){
+	
+		if(playerInput === computerSelection){
+			console.log("It's a Tie");
+}		else if(playerInput == 'rock'){ 
+			if (computerSelection =='scissors') {
+				console.log("Player wins");
+				return userscore ++;  
+			}else { 
+				if(computerSelection == 'paper'){
+					console.log("Sorry you lose");
+					return compscore ++; 
+				}
 			}
 		}
-	
+
+		else if(playerInput =='paper') {
+			if( computerSelection =='rock'){ 
+				console.log('Player Wins');
+				return userscore ++; 
+			}else {
+				if(computerSelection == 'scissors'){ 
+				console.log("Sorry You lose"); 
+				return compscore ++;
+				}
+			}
+		}
+		else if(playerInput =='scissors'){
+			if(computerSelection =='paper'){
+				console.log('Player wins'); 
+				return userscore ++;
+			}else{
+				if(computerSelection =='rock'){
+					console.log("Sorry You Lose");
+					return compscore ++; 
+				}
+	 		
+			} 
+		} 
+
+	}
+
 
